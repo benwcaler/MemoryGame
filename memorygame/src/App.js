ffad62be6985e08bd5329f1cb9146fc6cad5fe06
Ben <b.wayne@me.com>
2018-06-12 19:52:02 -0600

    initial commit

---
+++ memorygame/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h2>The Memory Game Where You Click on Stuff and it Moves Around!</h2>
+        </header>
+        <div id="imageContainer">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
